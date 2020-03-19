7279e2cbb41402983e4a34604174e2cb653e0e50
gonzalojbueno <gonzalojbueno@gmail.com>
2020-03-19 13:04:37 -0300

    Updates

---
+++ precache-manifest.7f008cb83a43589adcb231b028656816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9232c980391d95b514bfb032a7abb630",
+    "url": "/gonzalojbueno/portfolio-cv-app/index.html"
+  },
+  {
+    "revision": "74f498ace04e1edcea6c",
+    "url": "/gonzalojbueno/portfolio-cv-app/static/css/main.c8f1e3a5.chunk.css"
+  },
+  {
+    "revision": "f00bbae156c8679f9d6b",
+    "url": "/gonzalojbueno/portfolio-cv-app/static/js/2.f3306f38.chunk.js"
+  },
+  {
+    "revision": "cfae5b7d83e903e3eac5d300e4eb8849",
+    "url": "/gonzalojbueno/portfolio-cv-app/static/js/2.f3306f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f498ace04e1edcea6c",
+    "url": "/gonzalojbueno/portfolio-cv-app/static/js/main.d16a8a77.chunk.js"
+  },
+  {
+    "revision": "df32d6053a3c593644b8",
+    "url": "/gonzalojbueno/portfolio-cv-app/static/js/runtime-main.3e4349e9.js"
+  }
+]);
